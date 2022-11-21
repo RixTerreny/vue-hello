3fd9af1314c2a80dec6335a0cccb303369746d3f
RixTerreny <riccardotrave43@gmail.com>
2022-11-21 23:45:03 +0100

    bonus added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            message: "Vue.js"
+            message: "Vue.js",
+            luogo: "Faro",
+            classiRiccardo:"mt-3 text-white shadow bg-dark py-2 mb-4",
+            scelto: true,
+            immagine: "img/img.jpg"
         }
     }
 }).mount("#app");
